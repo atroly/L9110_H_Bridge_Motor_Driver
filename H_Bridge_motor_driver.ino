9b66405844af2bd61441db70f9c2b3eb42af472a
Chris <25781272+atroly@users.noreply.github.com>
2017-07-23 13:33:29 +0100

    Add GNU GPL

--- H_Bridge_motor_driver.ino
+++ H_Bridge_motor_driver.ino
@@ -1,8 +1,20 @@
 /*
-  L9110 motor driver for automatic watch winder
-  Chris Taylor
-  21 July 2017
-*/
+ * L9110 motor driver for automatic watch winder
+ * Copyright (C) 2017 Chris Taylor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+ */
 
 #define SECONDS 10      // seconds to run for each step in sequence
 #define DEFAULT_PWM 192 // default duty cycle (255=full speed)
